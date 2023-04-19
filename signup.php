30838286c97bec8885ab5e5a8f7c0eab655bb6d4
Sarah Gilbreath <thenoiseinspace@gmail.com>
2023-04-19 16:01:31 -0400

    signup validation and error messaging

--- signup.php
+++ signup.php
@@ -14,7 +14,33 @@ include_once 'header.php';
                     <button type="submit" name="submit">Sign up</button>
                 </form>
                 </div>
+                <?php
+                if(isset($_GET["error"])){
+                    if($_GET["error"] == "emptyinput"){
+                        echo "<p>Please fill in all fields</p>";
+                    }
+                    else if ($_GET["error"] == "invaliduid"){
+                        echo "<p>Choose a different username </p>";
+                    }
+                    else if ($_GET["error"] == "invalidemail"){
+                        echo "<p>Choose a proper email </p>";
+                    }
+                    else if ($_GET["error"] == "pwdmismatch"){
+                        echo "<p>Passwords don't match </p>";
+                    }
+                    else if ($_GET["error"] == "stmtfailed"){
+                        echo "<p>Something went wrong. Please try again </p>";
+                    }
+                    else if ($_GET["error"] == "usernametaken"){
+                        echo "<p>Choose a different username </p>";
+                    }
+                    else if ($_GET["error"] == "none"){
+                        echo "<p>Thank you for signing up </p>";
+                    }
+                }   
+            ?>
             </section>
+
 
 
 
