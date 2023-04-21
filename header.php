693d19a4a1c76edef4a26fc8758dae3a67390bf7
Sarah Gilbreath <thenoiseinspace@gmail.com>
2023-04-21 09:43:03 -0400

    logout, session destroy and greeting message

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
                     <?php
                         if (isset($_SESSION["useruid"])){
                             echo "<li><a href='profile.php'>Profile</a></li>"; 
-                            echo "<li><a href='logout.php'>Log out</a></li>";
+                            echo "<li><a href='includes/logout.inc.php'>Log out</a></li>";
                         }
                         else {
                            echo "<li><a href='signup.php'>Sign up</a></li>";
--- index.php
+++ index.php
@@ -3,6 +3,11 @@ include_once 'header.php';
 ?>
 
             <section class="index-intro">
+                <?php
+                        if (isset($_SESSION["useruid"])){
+                            echo "<p>Hello " .$_SESSION["useruid"] . "</p>"; 
+                        }
+                        ?>
                 <h1> This is an introduction</h1>
                 <p>explanitory intro paragraph</p>
             </section>
