7457592ba404d6655f35cec2c17768fb87a9da49
Sarah Gilbreath <thenoiseinspace@gmail.com>
2023-05-01 16:04:41 -0400

    initializing files for profile page

--- profile.php
+++ profile.php
@@ -2,33 +2,12 @@
 include_once 'header.php';
 ?>
 
-            <section class="index-intro">
-                <?php
-                        if (isset($_SESSION["useruid"])){
-                            echo "<p>Hello " .$_SESSION["useruid"] . "</p>"; 
-                        }
-                        ?>
-                <h1> This is an introduction</h1>
-                <p>explanitory intro paragraph</p>
-            </section>
+<h3>Profile</h3>
+<p>A little about yourself</p>
 
-            <section class="index-categories">
-                <h2>Basic Categories</h2>
-                <div class="index-categories-list">
-                    <div>
-                        <h3>Fun stuff</h3>
-                    </div>
-                    <div>
-                        <h3>Serious stuff</h3>
-                    </div>
-                    <div>
-                        <h3>Exciting stuff</h3>
-                    </div>
-                    <div>
-                        <h3>Boring stuff</h3>
-                    </div>
-                </div>
-            </section>
+
+</body>
+</html>
 
 <?php
 include_once 'footer.php';
